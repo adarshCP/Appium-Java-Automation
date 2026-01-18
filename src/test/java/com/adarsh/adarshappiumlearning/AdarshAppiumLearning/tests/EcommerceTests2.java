0be8a2f4fa562fb5ac8dbefdbe9dee923022db80
adarshCP <1adarshcp@gmail.com>
2026-01-19 02:18:34 +0530

    update

--- src/test/java/com/adarsh/adarshappiumlearning/AdarshAppiumLearning/tests/EcommerceTests2.java
+++ src/test/java/com/adarsh/adarshappiumlearning/AdarshAppiumLearning/tests/EcommerceTests2.java
@@ -13,7 +13,7 @@ import com.adarsh.adarshappiumlearning.AdarshAppiumLearning.pages.common.PLPPage
 
 public class EcommerceTests2 extends BaseTest {
 
-    private static final String PRODUCT_NAME = "Sauce Labs Onesie";
+    private static final String PRODUCT_NAME ="Sauce Labs Onesie";
 
     @BeforeMethod(alwaysRun = true)
     public void login() {
